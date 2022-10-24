2446f0a68fb9ecfaa21eb4fa16706605bf3b6cd0
MateK23 <mate.warface@gmail.com>
2022-10-24 20:08:30 +0400

    streaming examples

---
+++ src/com/matek/StreamsCode1.java
@@ -0,0 +1,40 @@
+package com.matek;
+
+import java.io.*;
+
+public class StreamsCode1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    File file1 = new File("");
+    File file2 = new File("");
+    InputStream inputStream = new FileInputStream(file1);
+    OutputStream outputStream = new FileOutputStream(file2);
+    byte[] buffer = new byte[(int) file1.length()];
+
+    inputStream.read(buffer);
+    outputStream.write(buffer);
+
+     */
+
+
+    /*
+    Reader reader = new FileReader("D:\\Programming\\JVM\\FileTest");
+
+    int read;
+
+    while((reader.read())!=-1)
+
+    {
+
+        read = reader.read();
+
+        char c = (char) read;
+        System.out.print(c);
+    }
+     */
+
+}
